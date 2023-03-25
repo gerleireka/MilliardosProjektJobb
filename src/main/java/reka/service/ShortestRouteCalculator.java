4ed3d1c034ec893d03151f4be88a88f4d044c835
Gerlei Réka <reka.gerlei@gmail.com>
2023-03-25 20:31:50 +0100

    kiskacsa

--- src/main/java/reka/service/ShortestRouteCalculator.java
+++ src/main/java/reka/service/ShortestRouteCalculator.java
@@ -8,14 +8,29 @@ import java.util.List;
 
 public class ShortestRouteCalculator {
     public List<Location> calculateShortestRoute (Location start, Location stop, LocationGraph myMap){
-       List<Location> result=new ArrayList<>();
-       result.add(start);
-       List<Location> currentNeighbours=start.getAdjacencyList();
-        for (int i = 0; i < currentNeighbours.size(); i++) {
-            currentNeighbours.get(i);
+
+
+        Graph terkep=new Graph(myMap.getLocationList().size());
+        for (int i=0; i<myMap.getLocationList().size();i++){
+            Location location=myMap.getLocationList().get(i);
+            for (int j=0; j<location.getAdjacencyList().size();j++){
+                int szomszedszama=findszomszedszama(location.getAdjacencyList().get(j),myMap);
+              terkep.addEdge(i,szomszedszama);
+            }
 
         }
         return null;
     }
-    private
+
+    private int findszomszedszama(Location location,LocationGraph myMap) {
+        myMap.getLocationList();
+        for (int i = 0; i < myMap.getLocationList().size(); i++) {
+            if (myMap.getLocationList().get(i).getName() == location.getName()) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
 }
