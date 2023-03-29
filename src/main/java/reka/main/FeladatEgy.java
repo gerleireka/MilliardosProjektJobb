5d22bf9256e5a5492a2be8e17087993ab7424ded
Gerlei Réka <reka.gerlei@gmail.com>
2023-03-29 21:43:46 +0200

    terkep

--- src/main/java/reka/main/FeladatEgy.java
+++ src/main/java/reka/main/FeladatEgy.java
@@ -1,30 +1,36 @@
 package reka.main;
 
 import reka.model.Location;
+import reka.model.LocationGraph;
 
 import java.util.Collections;
 import java.util.List;
 
 public class FeladatEgy {
-    public void static void main(String[] args) {
+    public static void main(String[] args) {
         Location oktogon=new Location("Oktogon", Collections.emptyList());
         Location nyugatiTer=new Location("Nyugati Ter", Collections.emptyList());
         Location deakTer=new Location("Deak Ter", Collections.emptyList());
         Location borarosTer=new Location("Boraros Ter", Collections.emptyList());
         Location moriczZsigmondKorter=new Location("Moricz Zsigmond Korter", Collections.emptyList());
-        Location kalvinTer=new Location("Kalvin Ter", Collections.emptyList());
-        List deakTerConnections=List.of(nyugatiTer,oktogon);
+        Location kalvinTer= new Location("Kalvin Ter", Collections.emptyList());
+        List<Location> deakTerConnections=List.of(nyugatiTer,oktogon);
         deakTer.setAdjacencyList(deakTerConnections);
-        List nyugatiTerConnections=List.of(deakTer,oktogon,kalvinTer);
+        List<Location> nyugatiTerConnections=List.of(deakTer,oktogon,kalvinTer);
         nyugatiTer.setAdjacencyList(nyugatiTerConnections);
-        List borarosTerConnections=List.of(moriczZsigmondKorter,oktogon);
+        List<Location> borarosTerConnections=List.of(moriczZsigmondKorter,oktogon);
         borarosTer.setAdjacencyList(borarosTerConnections);
-        List oktogonConnections=List.of(nyugatiTer,deakTer,borarosTer,kalvinTer);
+        List<Location> oktogonConnections=List.of(nyugatiTer,deakTer,borarosTer,kalvinTer);
         oktogon.setAdjacencyList(oktogonConnections);
-        List moriczZsigmondKorterConnections=List.of(borarosTer,kalvinTer);
+        List<Location> moriczZsigmondKorterConnections=List.of(borarosTer,kalvinTer);
         moriczZsigmondKorter.setAdjacencyList(moriczZsigmondKorterConnections);
-        List kalvinTerConnections=List.of(nyugatiTer,oktogon);
+        List<Location> kalvinTerConnections=List.of(nyugatiTer,oktogon);
         kalvinTer.setAdjacencyList(kalvinTerConnections);
+//hozzaadjuk a terkephez
+        List<Location> helyekListaja=List.of(oktogon,nyugatiTer,deakTer,borarosTer,moriczZsigmondKorter);
+        helyekListaja.add(kalvinTer);
+        LocationGraph elsoTerkep= new LocationGraph(helyekListaja);
+        //szemelyek
 
     }
 }
